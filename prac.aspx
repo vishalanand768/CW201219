114ba620abd345a43dc785f023922ccf80cfe96c
vishalanand768 <vishalanand768@yahoo.com>
2019-12-20 20:59:06 +0530

    Add project files.

---
+++ prac.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="prac.aspx.cs" Inherits="CW201219.prac" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Name :</td>
+                    <td><asp:TextBox ID="txtname" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>Qualification :</td>
+                    <td><asp:DropDownList ID="ddlqual" runat="server"></asp:DropDownList></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td><asp:Button ID="btnsave" runat="server" Text="Save" OnClick="btnsave_Click"></asp:Button></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td><asp:GridView ID="grd" runat="server" OnRowCommand="grd_RowCommand" AutoGenerateColumns="false">
+
+                         <Columns>
+
+                          <asp:TemplateField HeaderText="Name">
+                              <ItemTemplate>
+                                  <%#Eval("name") %>
+                              </ItemTemplate>
+                          </asp:TemplateField>
+
+                           <asp:TemplateField HeaderText="Qualification">
+                              <ItemTemplate>
+                                  <%#Eval("qualification") %>
+                              </ItemTemplate>
+                          </asp:TemplateField>
+
+                              <asp:TemplateField HeaderText="Action">
+                        <ItemTemplate>
+                           <asp:Button ID="btndelete" runat="server" Text="Delete" CommandName="A" CommandArgument='<%#Eval("id") %>' />
+                           <asp:Button ID="btnedit" runat="server" Text="Edit" CommandName="B" CommandArgument='<%#Eval("id") %>' />
+                        </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+
+                        </asp:GridView></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ prac.aspx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace CW201219
+{
+    public partial class prac : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["xyz"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                get_bindqual();
+                BindData();
+            }
+        }
+
+        public void get_bindqual()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_main", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@action", "qualbind");
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+
+            ddlqual.DataValueField = "qid";
+            ddlqual.DataValueField = "qname";
+            ddlqual.DataSource = dt;
+            ddlqual.DataBind();
+
+            ddlqual.Items.Insert(0, new ListItem("--Select--", "0"));
+        }
+        public void BindData()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_main", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@action","select");
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+
+            grd.DataSource = dt;
+            grd.DataBind();
+        }
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            if (btnsave.Text == "Save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_main", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "insert");
+                cmd.Parameters.AddWithValue("@name", txtname.Text);
+                cmd.Parameters.AddWithValue("@qualification", ddlqual.SelectedValue);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindData(); 
+            }
+            else if (btnsave.Text == "Update")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_main", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "update");
+                cmd.Parameters.AddWithValue("@id", ViewState["pp"]);
+                cmd.Parameters.AddWithValue("@name", txtname.Text);
+                cmd.Parameters.AddWithValue("@qualification", ddlqual.SelectedValue);
+               
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindData();
+                btnsave.Text = "Save";
+            }
+
+        }
+
+        protected void grd_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "A")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_main", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "delete");
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindData(); 
+            }
+            else if (e.CommandName == "B")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_main", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "edit");
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                con.Close();
+                txtname.Text = dt.Rows[0][1].ToString();
+                ddlqual.SelectedValue = dt.Rows[0][2].ToString();
+                btnsave.Text = "Update";
+                ViewState["pp"] = e.CommandArgument;
+            }
+        }
+    }
+}
